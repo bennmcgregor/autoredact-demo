23fb71ba26a2d78732bf6efed83dd2702c162ede
Kate Granstrom <kategranstrom2000@gmail.com>
2023-03-19 17:05:03 -0400

    file picker and flask app endpoint

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+app = Flask(__name__)
+	
+@app.route('/redactor', methods = ['POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      return 'file uploaded successfully'
+		
+if __name__ == '__main__':
+   app.run(debug = True)
