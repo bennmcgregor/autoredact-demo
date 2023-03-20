5d098dfe8908708431a1919404b03745e492b692
Juliana Zadarko <jhzadark@edu.uwaterloo.ca>
2023-03-20 00:40:24 -0400

    Redacts in .docx

--- backend/app.py
+++ backend/app.py
@@ -3,13 +3,58 @@ from flask import Flask, request
 from docx import Document
 from pathlib import Path
 import subprocess
+from docx import Document
+import spacy
+from spacy.tokens import DocBin
 
 app = Flask(__name__)
+
+def redact_doc():
+   nlp = spacy.blank("en")
+   doc_bin = DocBin().from_disk("output.spacy")
+   docs = list(doc_bin.get_docs(nlp.vocab))
+
+   redact_paras = {}
+
+   for i,doc in enumerate(docs):
+      if doc.spans:
+         for t in doc.spans['sc']:
+               if(not (i in redact_paras)):
+                  redact_paras[i] = []
+               redact_paras[i].append(t)
+
+   data_doc = Document("data.docx")
+   for i in redact_paras:
+      para_doc = nlp(data_doc.paragraphs[i].text)
+      tokens = [x for x in para_doc]
+      spaces = [x.whitespace_ for x in para_doc]
+      #tokens = re.findall(r"[\w']+|[.,!?;]", data_doc.paragraphs[i].text)
+      # print(tokens)
+      for span_r in redact_paras[i]:
+         start = span_r.start
+         while start < span_r.end:
+               tokens[start] = "XXXX"
+               start += 1
+      output = ""
+      for token_i in range(len(tokens)):
+         output += f'{tokens[token_i]}{spaces[token_i]}'
+      data_doc.paragraphs[i].text = output
+   data_doc.save('redacted.docx')
+
+# test_bin = DocBin().from_disk("../data/paired_data/test_data.spacy")
+# tests = list(test_bin.get_docs(nlp.vocab))
+# print("HIII")
+# for i,test in enumerate(tests):
+#         #print(test)
+#         print(test.spans["sc"])
 	
 @app.route('/redactor', methods = ['POST'])
 def upload_file():
    if request.method == 'POST':
       f = request.files['file']
+
+      f.save('data.docx')
+
       doc = Document(f)
       paras = []
       for i, p in enumerate(doc.paragraphs):
@@ -18,12 +63,15 @@ def upload_file():
              "text": p.text
           })
 
+
       with open("data.jsonl", "w") as output:
         while len(paras) > 0:
             para = paras.pop(0)
             output.write(json.dumps(para) + "\n")
       
-      subprocess.run(["python", "-m", "spacy", "apply", "./models/legal_bert/model-best", "data.jsonl", "output.spacy"])
+      subprocess.run(["python", "-m", "spacy", "apply", "./models/legal_bert/model-best", "data.jsonl", "output.spacy", "--force"])
+
+      redact_doc()
 
       return 'file uploaded successfully'
 		
---
+++ redact_doc_vis.py
@@ -0,0 +1,89 @@
+from docx import Document
+import spacy
+from spacy.tokens import DocBin
+import re
+
+nlp = spacy.blank("en")
+doc_bin = DocBin().from_disk("backend/output.spacy")
+docs = list(doc_bin.get_docs(nlp.vocab))
+print(len(docs))
+# print(docs[0])
+# print(len(docs[0]))
+# print(docs[0])
+
+# class redactW:
+#       def __init__(self, id, span):
+#             self.span = span
+#             self.docId = id
+
+redact_paras = {}
+
+for i,doc in enumerate(docs):
+    if doc.spans:
+        # print(doc)
+        # for t in doc:
+        #       print(t)
+        for t in doc.spans['sc']:
+            if(not (i in redact_paras)):
+               redact_paras[i] = []
+            redact_paras[i].append(t)
+            # print(doc.spans['sc'])
+            # print(i)
+            # print(t)
+            # print(t.start)
+            # print(t.end)
+            # print("-------------------")
+            # break
+#     break
+        # print("DEBUGG-------------")
+        # print(doc)
+#         #print(doc)
+#         print(len(doc.spans))
+
+data_doc = Document("backend/data.docx")
+for i in redact_paras:
+    print(i)
+    para_doc = nlp(data_doc.paragraphs[i].text)
+    tokens = [x for x in para_doc]
+    spaces = [x.whitespace_ for x in para_doc]
+    #tokens = re.findall(r"[\w']+|[.,!?;]", data_doc.paragraphs[i].text)
+    # print(tokens)
+    for span_r in redact_paras[i]:
+        start = span_r.start
+        while start < span_r.end:
+            tokens[start] = "XXXX"
+            start += 1
+    output = ""
+    for token_i in range(len(tokens)):
+        output += f'{tokens[token_i]}{spaces[token_i]}'
+    data_doc.paragraphs[i].text = output
+    # print(output)
+    # print(nlp(tokens))
+
+data_doc.save('redacted.docx')
+
+# print(data_doc.paragraphs[49].text)
+# for i in range(len(data_doc.paragraphs[49].text.split(" "))):
+#     print(i)
+#     print(data_doc.paragraphs[49].text.split(" ")[i])
+
+# print(len(data_doc.paragraphs))
+# paras = []
+# for i, p in enumerate(doc.paragraphs):
+#     for ir, pr in enumerate(doc.paragraphs):
+#     print(p.text)
+        # paras.append({
+        # "i": i,
+        # "text": p.text
+        # })
+
+
+
+
+
+# test_bin = DocBin().from_disk("../data/paired_data/test_data.spacy")
+# tests = list(test_bin.get_docs(nlp.vocab))
+# print("HIII")
+# for i,test in enumerate(tests):
+#         #print(test)
+#         print(test.spans["sc"])
