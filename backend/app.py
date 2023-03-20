bb35fc71f30a1766ebb1b45c11a48f8506495f43
Kate Granstrom <kategranstrom2000@gmail.com>
2023-03-19 22:08:39 -0400

    can apply to local model

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,30 @@
+import json
 from flask import Flask, request
+from docx import Document
+from pathlib import Path
+import subprocess
+
 app = Flask(__name__)
 	
 @app.route('/redactor', methods = ['POST'])
 def upload_file():
    if request.method == 'POST':
       f = request.files['file']
+      doc = Document(f)
+      paras = []
+      for i, p in enumerate(doc.paragraphs):
+          paras.append({
+             "i": i,
+             "text": p.text
+          })
+
+      with open("data.jsonl", "w") as output:
+        while len(paras) > 0:
+            para = paras.pop(0)
+            output.write(json.dumps(para) + "\n")
+      
+      subprocess.run(["python", "-m", "spacy", "apply", "./models/en_trf_mlt_label_v4_1000000/model-last", "data.jsonl", "output.spacy"])
+
       return 'file uploaded successfully'
 		
 if __name__ == '__main__':
